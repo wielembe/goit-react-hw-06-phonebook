5fe9383a1579b76aac410f56ea81f86d0762430a
wielembe <e.wielemborek@gmail.com>
2023-12-26 21:30:03 +0100

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-
+import { v4 as uuidv4 } from 'uuid';
 import css from './App.module.css';
 
 export const App = () => {
   return (
     <div className={css.container}>
       <h1 className={css.titles}>Phonebook</h1>
-      <ContactForm />
+      <ContactForm generateId={uuidv4()} />
       <h2 className={css.titles}>Contacts</h2>
       <Filter />
       <ContactList />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import css from './ContactForm.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'components/redux/contactsSlice';
@@ -26,25 +27,27 @@ export const ContactForm = () => {
   };
   return (
     <form className={css.contactForm} onSubmit={handleSubmit}>
-      <h3 className={css.subtitle}>Name</h3>
+      <label htmlFor={uuidv4()}>Name</label>
       <input
         className={css.contactForm__input}
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+([ -'][a-zA-Zа-яА-Я]+)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        id={uuidv4()}
         required
       />
-      <h3 className={css.subtitle}>Number </h3>
+
+      <label htmlFor={uuidv4()}>Number </label>
       <input
         className={css.contactForm__input}
         type="tel"
         name="number"
         pattern="^[+]?[0-9 \u0028\u0029\u002D]*$"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        id={uuidv4()}
         required
       />
-
       <button type="submit">Add contact</button>
     </form>
   );
