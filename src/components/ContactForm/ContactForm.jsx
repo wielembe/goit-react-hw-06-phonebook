5fc732fe121300dd9642c74996e11c3d31dafa90
wielembe <e.wielemborek@gmail.com>
2023-12-26 17:53:22 +0100

    deploy test

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 import css from './ContactForm.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'components/redux/contactsSlice';
@@ -27,24 +26,22 @@ export const ContactForm = () => {
   };
   return (
     <form className={css.contactForm} onSubmit={handleSubmit}>
-      <label htmlFor={uuidv4()}>Name</label>
+      <h3 className={css.subtitle}>Name</h3>
       <input
         className={css.contactForm__input}
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+([ -'][a-zA-Zа-яА-Я]+)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        id={uuidv4()}
         required
       />
-      <label htmlFor={uuidv4()}>Number </label>
+      <h3 className={css.subtitle}>Number </h3>
       <input
         className={css.contactForm__input}
         type="tel"
         name="number"
         pattern="^[+]?[0-9 \u0028\u0029\u002D]*$"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        id={uuidv4()}
         required
       />
 
